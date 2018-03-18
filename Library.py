b958d1373063a9784527adcd394f0319d29614ca
Saurabh Mudgal <smsaurabhmudgal@gmail.com>
2018-03-19 04:42:44 +0800

    Create Library.py

---
+++ Library.py
@@ -0,0 +1,5 @@
+# Below is an example of a library. This is a code to display the version of the python software
+
+import platform
+
+print('This is python version ' + str(platform.python_version())
