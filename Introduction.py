6099ca74ab8a6f46419683b8dadfdc9de2ed621e
Saurabh Mudgal <smsaurabhmudgal@gmail.com>
2018-03-19 03:53:18 +0800

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -1,8 +1,8 @@
+age = 5+(8%3)-3+(3*10)/2
 greetings = "Welcome to IEEE Python Workshop 2018 edition. It's my pleasure to conduct today's workshop for you."
 name = "Saurabh Mudgal"
 major = "mechanical engineering"
-age = 19
 
 print(greetings)
-print("My name is " + name)
-print("I am " + str(age) + " years old and am majoring in " + major)
+print("My name is " + name + ".")
+print("I am " + str(age) + " years old and am majoring in " + major + ".")
