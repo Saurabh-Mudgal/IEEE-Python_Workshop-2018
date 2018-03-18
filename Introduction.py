c06b554b87cfc6a0659926e254e56c219f6b998d
Saurabh Mudgal <smsaurabhmudgal@gmail.com>
2018-03-18 23:56:49 +0800

    Create Introduction.py

---
+++ Introduction.py
@@ -0,0 +1,8 @@
+greetings = "Welcome to IEEE Python Workshop 2018 edition. It's my pleasure to conduct today's workshop for you."
+name = "Saurabh Mudgal"
+major = "mechanical engineering"
+age = 19
+
+print(greeting)
+print("My name is " + name)
+print("I am " + age + "years old and am majoring in " + major)
